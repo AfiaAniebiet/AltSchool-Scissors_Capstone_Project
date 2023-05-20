7394b8babd981ff7a4c213a8407ce8613bbc169e
AfiaAniebiet <leeboy4u2002@yahoo.com>
2023-05-20 20:43:21 +0100

    configuration setup

--- server/dist/config/default.js
+++ server/dist/config/default.js
@@ -2,5 +2,16 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.default = {
     PORT: 4000,
-    mongo_uri: 'mongodb://localhost:27017/url-shortener',
+    mongo_uri: 'mongodb://127.0.0.1:27017/url-shortener',
 };
+// import nodeConfig from 'config';
+// interface Config {
+//   PORT: number;
+//   mongo_uri: string;
+// }
+// const config: Config = {
+//   PORT: nodeConfig.get<number>('PORT'),
+//   mongo_uri: nodeConfig.get<string>('mongo_uri'),
+// };
+// export default config;
+//# sourceMappingURL=default.js.map
